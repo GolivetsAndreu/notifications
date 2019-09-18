7da8a0f75995057200338145107e36580d4d8ba8
GolivetsAndreu <golivets18042001@ukr.net>
2019-09-18 10:40:57 +0200

    Added logs directory

---
+++ migrations/20190916140431-create-notification.js
@@ -0,0 +1,40 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Notifications', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      subject: {
+        type: Sequelize.STRING
+      },
+      body: {
+        type: Sequelize.STRING
+      },
+      recipient: {
+        type: Sequelize.STRING
+      },
+      template: {
+        type: Sequelize.STRING
+      },
+      isSend: {
+        type: Sequelize.BOOLEAN,
+        default: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Notifications');
+  }
+};
